309647349c2867eda4c63ebd9a52e2825eeb2ee9
Elliot Kuok <elliotckuok@gmail.com>
2023-08-01 14:58:30 -0700

    Finish phase 3

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
@@ -1,2 +1,27 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { createContext, useContext, useState } from "react";
+
+export const ClimateContext = createContext();
+
+export const useClimate = () => useContext(ClimateContext);
+
+export const ClimateProvider = ({children}) => {
+    const [temperature, setTemperature] = useState(50);
+    const [humidity, setHumidity] = useState(40);
+
+    return (
+        <ClimateContext.Provider
+        value={{
+            temperature,
+            setTemperature,
+            humidity,
+            setHumidity
+        }}
+        >
+            {children}
+        </ClimateContext.Provider>
+
+    )
+}
